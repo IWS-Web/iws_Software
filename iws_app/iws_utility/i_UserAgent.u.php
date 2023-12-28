759ab4496c55f9833c8d06fdef6b077593c2d3e2
IWS-Web <ceoc0d3x@gmail.com>
2023-12-28 23:31:32 +0100

    Update i_UserAgent.u.php

--- iws_app/iws_utility/i_UserAgent.u.php
+++ iws_app/iws_utility/i_UserAgent.u.php
@@ -1,49 +1,49 @@
 <?php
 
-class UserAgentChecker {
-    private $userAgents;
+    namespace iws_utility;
 
-    public function __construct($jsonUrl) {
-        $this->userAgents = $this->loadUserAgents($jsonUrl);
-    }
+    class UserAgent {
+        private $userAgents;
 
-    private function loadUserAgents($jsonUrl) {
-        $json = file_get_contents($jsonUrl);
-        return json_decode($json, true);
-    }
+        public function __construct($jsonUrl) {
+            $this->userAgents = $this->loadUserAgents($jsonUrl);
+        }
 
-    private function isBotUserAgent($userAgent, $instances) {
-        foreach ($instances as $instance) {
-            if (stripos($userAgent, $instance) !== false) {
-                return true;
+        private function loadUserAgents($jsonUrl) {
+            $json = file_get_contents($jsonUrl);
+            return json_decode($json, true);
+        }
+
+        private function isBotUserAgent($userAgent, $instances) {
+            foreach ($instances as $instance) {
+                if (stripos($userAgent, $instance) !== false) {
+                    return true;
+                }
             }
-        }
-        return false;
-    }
-
-    public function checkUserAgent($userAgent, $urlParameter) {
-        if (!empty($urlParameter)) {
-            // Wenn ein URL-Parameter vorhanden ist, Header mit Fehlercode und Fehlermeldung senden
-            header("HTTP/1.1 403 Forbidden");
-            echo "Bots sind gesperrt.";
-            exit;
+            return false;
         }
 
-        foreach ($this->userAgents as $agent) {
-            if ($this->isBotUserAgent($userAgent, $agent['instances'])) {
-                // Webcrawler gefunden, Header mit Fehlercode und Fehlermeldung senden
+        public function checkUserAgent($userAgent, $urlParameter) {
+            if (!empty($urlParameter)) {
+                // Wenn ein URL-Parameter vorhanden ist, Header mit Fehlercode und Fehlermeldung senden
                 header("HTTP/1.1 403 Forbidden");
-                echo "Webcrawler sind gesperrt.";
+                echo "Forbidden";
                 exit;
             }
 
-            // Hier können Sie weitere Überprüfungen für Muster und Instanzen implementieren
-        }
+            foreach ($this->userAgents as $agent) {
+                if ($this->isBotUserAgent($userAgent, $agent['instances'])) {
+                    // Webcrawler gefunden, Header mit Fehlercode und Fehlermeldung senden
+                    header("HTTP/1.1 403 Forbidden");
+                    echo "Forbidden";
+                    exit;
+                }
+            }
 
-        // Hier können Sie den normalen Ablauf fortsetzen, da kein Bot erkannt wurde
-        echo "Normales Verhalten.";
+            // Hier können Sie den normalen Ablauf fortsetzen, da kein Bot erkannt wurde
+            echo "Normales Verhalten.";
+        }
     }
-}
 
 
 ?>
