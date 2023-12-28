34a79237f1ff0b1f097f4c551ed484f226c544bd
IWS-Web <ceoc0d3x@gmail.com>
2023-12-28 20:36:23 +0100

    Create i_ServGuard.h.php

---
+++ iws_app/iws_handler/i_ServGuard.h.php
@@ -0,0 +1,25 @@
+<?php
+
+    class SecurityGuard {
+        private $allowedDomain;
+    
+        public function __construct($allowedDomain) {
+            $this->allowedDomain = $allowedDomain;
+        }
+    
+        public function enforceSecurity() {
+            $this->checkDomain();
+        }
+    
+        private function checkDomain() {
+            $currentHost = $_SERVER['HTTP_HOST'];
+    
+            if ($currentHost !== $this->allowedDomain) {
+                header("HTTP/1.1 403 Forbidden");
+                echo "Access forbidden.";
+                exit;
+            }
+        }
+    }
+
+    // $securityGuard = new SecurityGuard('deinedomain.com');
