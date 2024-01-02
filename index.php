52093f7d73d7f870f6655fab111815dd6157d521
IWS-Web <ceoc0d3x@gmail.com>
2024-01-02 17:24:15 +0100

    edit path | index.php

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php
 
     include(__DIR__."/iws_app/iws_config/i_Config.php");
-    include(__DIR__."/iws_app/iws_handler/i_Autoloader.h.php");
+
+    include(__DIR__."/iws_app/iws_vendor/i_Autoloader.php");
 
     $loader = new Autoloader(__DIR__."/iws_app");
     $loader->load();
